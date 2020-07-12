8a26d44d7f9f433b1788c8ca44679b64dcbc37d4
LeDang123 <wispeaha@gmail.com>
2020-07-12 08:34:30 +0700

    Event In VueCLI - 60%

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <app-header></app-header>
+    <app-header v-bind:title="title" v-on:click="changeTitle"></app-header>
     <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
     <hr/>
     <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer></app-footer>
+    <app-footer v-bind:title="title"></app-footer>
   </div>
 
 </template>
@@ -29,8 +29,16 @@ export default {
       {name: 'Tango', speciality: 'Conditionals', show:false},
       {name: 'Kami', speciality: 'Webpack', show:false},
       {name: 'Yoshi', speciality: 'Data Diggin', show:false}
-      ]
+      ], 
+
+      title: "Vue Ninjas",
   }),
+
+  methods: {
+    updateTitle: function(updatedTitle){
+      this.title = updatedTitle;
+    }
+  }
 }
 </script>
 
