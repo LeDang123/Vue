24aa162f34716568340fc86fe839d3abcd2283c2
LeDang123 <wispeaha@gmail.com>
2020-07-15 15:53:00 +0700

    Customer Directives

--- src/main.js
+++ src/main.js
@@ -9,7 +9,27 @@ import VueResource from "vue-resource";
 //Vue.config.productionTip = false;
 
 Vue.use(VueResource);
-export const bus = new Vue();
+
+//Custom Directives
+Vue.directive("rainbow",{
+  bind(el){
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+Vue.directive("Theme",{
+  bind(el,binding){
+    if(binding.value == "wide"){
+      el.style.maxWidth = "960px";
+    }else if (binding.value == 'narrow'){
+      el.style.maxWidth = "560px";
+    }
+    if (binding.arg == "column"){
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+});
+//export const bus = new Vue();
 
 new Vue({
   //router,
