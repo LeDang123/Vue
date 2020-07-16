4880d336faf4e98c4dabd2c7c5368ede0fd775ca
LeDang123 <wispeaha@gmail.com>
2020-07-16 11:18:21 +0700

    Conflict Update

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,69 +1,61 @@
 <template>
-  <div v-theme:column="'wide'" id="show-blogs">
-    <h1> All Blog Articles - GET REQUEST </h1>
-    <input type="text" v-model="search" placeholder="search blogs" />
-    <div v-for="blog in filterBlogs" v-bind:key="blog" class="single-blog">
-        <h2 v-rainbow> {{ blog.title | to-uppercase }}</h2>
-        <article>{{ blog.body | snippet }}</article>
-    </div>
-<br/>
-  </div>
+   <div v-theme:column="'wide'" id="show-blogs">
+      <h1> All Blog Articles - GET REQUEST </h1>
+      <input type="text" v-model="search" placeholder="search blogs" />
+      <div v-for="blog in filterBlogs" v-bind:key="blog" class="single-blog">
+         <h2 v-rainbow> {{ blog.title | to-uppercase }}</h2>
+         <article>{{ blog.body | snippet }}</article>
+      </div>
+      <br/>
+   </div>
 </template>
-
 <script>
-import searchMinxins from "../mixins/searchMinxins";
-
-export default {
- 
-  data: () => ({
-    blogs: [],
-    search: ''
-  }),
-
-  methods:{
-
-  },
-  created(){
-      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-          console.log(data);
-          this.blogs = data.body.slice(0,10);
-      })
-  },
-
-    computed: {
-
-        filterBlogs: function(){
-            return this.blogs.filter((blog) => {
-                return blog.title.match(this.search);
-            });
-        }
-    },
-    filters: {
-        toUppercase(value){
-            return value.toUpperCase();
-        }
-    },
-    directives: {
-        'rainbow':{
-           bind(el){
-            el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
-        }
-
-    }
-
-}
+   import searchMinxins from "../mixins/searchMinxins";
+   
+   export default {
+    
+     data: () => ({
+       blogs: [],
+       search: ''
+     }),
+   
+     methods:{
+   
+     },
+     created(){
+         this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+             console.log(data);
+             this.blogs = data.body.slice(0,10);
+         })
+     },
+   
+       computed: {
+   
+       },
+       mixins: [searchMinxins],
+       filters: {
+           toUppercase(value){
+               return value.toUpperCase();
+           }
+       },
+       directives: {
+           'rainbow':{
+              bind(el){
+               el.style.color = "#" + Math.random().toString().slice(2,8);
+     }
+           }
+       }
+   }
 </script>
-
 <style>
-#show-blogs{
-    max-width: 960px;
-    margin: 0 auto;
-}
-.single-blog{
-    padding: 20px;
-    margin: 20px 0;
-    box-sizing: border-box;
-    background: #eee;
-}
+   #show-blogs{
+   max-width: 960px;
+   margin: 0 auto;
+   }
+   .single-blog{
+   padding: 20px;
+   margin: 20px 0;
+   box-sizing: border-box;
+   background: #eee;
+   }
 </style>
